572ee68789a59fa1fda56eeec5681b44bc09dca9
anujherp <anujherp@gmail.com>
2025-05-13 14:33:35 +1000

    update

--- 1_setup.R
+++ 1_setup.R
@@ -1,5 +1,5 @@
 ## Script 1: This script contains a list of required R packages and 
-## paths that I will be using throughout the workflow.
+## paths that I will be using throughout the workflow....
 
 # Load and install required packages
 needed_packages <- c("tidyverse","sf", "raster","exactextractr", "terra", "rgdal", "geosphere",
@@ -26,7 +26,7 @@ sf::sf_use_s2(FALSE)
 sf_india <- read_sf(paste0(wd$shapefiles, "India_State_Boundary.shp")) %>%
   st_transform(4326)
 
-## Range polygon for the western ghats mountain range
+## Range polygon for the western ghats mountain range.
 wg <- read_sf(paste0(wd$shapefiles, "wg_border_region11.shp")) %>%
   st_set_crs(4326)
 
@@ -34,11 +34,11 @@ wg <- read_sf(paste0(wd$shapefiles, "wg_border_region11.shp")) %>%
 shoreline <- read_sf(paste0(wd$shapefiles, "shores_penindia.shp")) %>%
   st_crop(c(xmin = 68.1167 - 3, xmax = 97.4167 + 3, ymin = 8.0667 - 3, ymax = 37.1 + 3), shoreline) %>% st_set_crs(4326)
 
-## Geographic dataset of India's Protected Areas (as of 2016)
+## Geographic dataset of India's Protected Areas (as of 2016).
 protected_areas <- read_sf(paste0(wd$shapefiles, "pa_india.shp")) %>%
   st_union() %>% st_set_crs(4326)
 
-## Load raster layers (India DEM, built environment, Tree cover & loss)
+## Load raster layers (India DEM, built environment, Tree cover & loss).
 elevation <- raster(paste0(wd$rasters, "Ind_raster.tiff"))
 built <- terra::rast(paste0(wd$rasters, "Built_area_and_change.tif"))
 tree_cover <- raster(paste0(wd$rasters, "pen_india_treecover.tif"))
